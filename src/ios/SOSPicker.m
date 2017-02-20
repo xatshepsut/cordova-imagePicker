f8919db972fdcaf799ecf469415e1981a42f7f60
Aidela Karamyan <akaramyan@macadamian.com>
2017-02-20 17:48:12 +0400

    Updates SOSPicker code according to ELCImagePicker latest version

--- src/ios/SOSPicker.m
+++ src/ios/SOSPicker.m
@@ -45,64 +45,40 @@
 
 
 - (void)elcImagePickerController:(ELCImagePickerController *)picker didFinishPickingMediaWithInfo:(NSArray *)info {
-	CDVPluginResult* result = nil;
-	NSMutableArray *resultStrings = [[NSMutableArray alloc] init];
-    NSData* data = nil;
-    NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
-    NSError* err = nil;
-    NSFileManager* fileMgr = [[NSFileManager alloc] init];
-    NSString* filePath;
-    ALAsset* asset = nil;
-    UIImageOrientation orientation = UIImageOrientationUp;;
-    CGSize targetSize = CGSizeMake(self.width, self.height);
-	for (NSDictionary *dict in info) {
-        asset = [dict objectForKey:@"ALAsset"];
-        // From ELCImagePickerController.m
-
-        int i = 1;
-        do {
-            filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, @"jpg"];
-        } while ([fileMgr fileExistsAtPath:filePath]);
-        
-        @autoreleasepool {
-            ALAssetRepresentation *assetRep = [asset defaultRepresentation];
-            CGImageRef imgRef = NULL;
-            
-            //defaultRepresentation returns image as it appears in photo picker, rotated and sized,
-            //so use UIImageOrientationUp when creating our image below.
-            if (picker.returnsOriginalImage) {
-                imgRef = [assetRep fullResolutionImage];
-                orientation = (UIImageOrientation)[assetRep orientation];
-            } else {
-                imgRef = [assetRep fullScreenImage];
-            }
-            
-            UIImage* image = [UIImage imageWithCGImage:imgRef scale:1.0f orientation:orientation];
-            if (self.width == 0 && self.height == 0) {
-                data = UIImageJPEGRepresentation(image, self.quality/100.0f);
-            } else {
-                UIImage* scaledImage = [self imageByScalingNotCroppingForSize:image toSize:targetSize];
-                data = UIImageJPEGRepresentation(scaledImage, self.quality/100.0f);
-            }
-            
-            if (![data writeToFile:filePath options:NSAtomicWrite error:&err]) {
-                result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:[err localizedDescription]];
-                break;
-            } else {
-                [resultStrings addObject:[[NSURL fileURLWithPath:filePath] absoluteString]];
-            }
-        }
-
-	}
-	
-	if (nil == result) {
-		result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:resultStrings];
-	}
-
-	[self.viewController dismissViewControllerAnimated:YES completion:nil];
-	[self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
+  CDVPluginResult* result = nil;
+  NSMutableArray *resultStrings = [[NSMutableArray alloc] init];
+  NSString* docsPath = [NSTemporaryDirectory()stringByStandardizingPath];
+  NSFileManager* fileMgr = [[NSFileManager alloc] init];
+  NSString* filePath;
+  UIImage* image = nil;
+  
+  for (NSDictionary *dict in info) {
+    image = [dict objectForKey:UIImagePickerControllerOriginalImage];
+    // From ELCImagePickerController.m
+    
+    int i = 1;
+    do {
+      filePath = [NSString stringWithFormat:@"%@/%@%03d.%@", docsPath, CDV_PHOTO_PREFIX, i++, @"jpg"];
+    } while ([fileMgr fileExistsAtPath:filePath]);
+    
+    @autoreleasepool {
+      BOOL success = [UIImageJPEGRepresentation(image, 1.0) writeToFile:filePath atomically:YES];
+      if (!success) {
+        result = [CDVPluginResult resultWithStatus:CDVCommandStatus_IO_EXCEPTION messageAsString:@"Error writing image file"];
+        break;
+      } else {
+        [resultStrings addObject:[[NSURL fileURLWithPath:filePath] absoluteString]];
+      }
+    }
+  }
+  
+  if (nil == result) {
+    result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:resultStrings];
+  }
+  
+  [self.viewController dismissViewControllerAnimated:YES completion:nil];
+  [self.commandDelegate sendPluginResult:result callbackId:self.callbackId];
 }
-
 - (void)elcImagePickerControllerDidCancel:(ELCImagePickerController *)picker {
 	[self.viewController dismissViewControllerAnimated:YES completion:nil];
 	CDVPluginResult* pluginResult = nil;
